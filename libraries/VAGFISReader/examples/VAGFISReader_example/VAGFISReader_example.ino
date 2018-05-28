8f2182b269347a0df735ee7874e97c3f19a22883
Tomas kovacik <nail@nodomain.sk>
2018-05-28 11:23:47 +0200

    NAVI support

--- libraries/VAGFISReader/examples/VAGFISReader_example/VAGFISReader_example.ino
+++ libraries/VAGFISReader/examples/VAGFISReader_example/VAGFISReader_example.ino
@@ -23,16 +23,22 @@ void setup() {
 }
 
 void loop() {
-  if (radio_read.has_new_msg()) {    
-    for (uint8_t i = 1; i <radio_read.get_size()-1; i++) { //2st byte is "address"?, last is checksumm
+  if (radio_read.has_new_msg()) {
+    if (radio_read.msg_is_navi()) {
+     for (uint8_t i = 2; i <radio_read.get_size()-1; i++) { //1st byte is msg ID, second one is packet size, last is checksumm so we skip them
         Serial.write(radio_read.read_data(i));
+    }
+    } else {
+    for (uint8_t i = 1; i <radio_read.get_size()-1; i++) { //1st byte is msg ID, last is checksumm
+        Serial.write(radio_read.read_data(i));
+    }
     }
     Serial.println();
     radio_read.clear_new_msg_flag();
     last_update=millis();
   }
 
-  if((millis()-last_update)>5000){
+  if((millis()-last_update)>1000){
     radio_read.request();
     last_update=millis();
   }
