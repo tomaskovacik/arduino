f53fdff78ecefe49df24659a2d30469463b6dc9f
Tomas kovacik <nail@nodomain.sk>
2018-07-20 10:16:08 +0200

    library for F-6188 bluetooth module based on beken BK8000L bluetooth module

---
+++ libraries/F-6188/F6188.h
@@ -0,0 +1,143 @@
+#ifndef F6188_h
+#define F6188_h
+#include <Arduino.h>
+
+#define USE_SW_SERIAL 1
+
+#define DEBUG 0
+
+//cmds definitions:
+#define F6188_PAIRING_INIT "CA"            //  pairing   AT+CA\r\n     
+#define F6188_PAIRING_EXIT "CB"            //  Exit pairing  AT+CB\r\n     
+#define F6188_CONNECT_LAST_DEVICE "CC"     //  The last paired device connected  AT+CC\r\n     what this should do? connect to last connected device?
+#define F6188_DISCONNECT "CD"              //  disconnect  AT+CD\r\n     
+#define F6188_CALL_ANSWARE "CE"            //  Answer the call   AT+CD\r\n     
+#define F6188_CALL_REJECT "CF"             //  reject a call   AT+CF\r\n     
+#define F6188_CALL_HANGUP "CG"             //  Hang up   AT+CG\r\n     
+#define F6188_CALL_REDIAL "CH"             //  redial  AT+CH\r\n     last called number?
+#define F6188_VOLUME_UP "CK"               //  volume up   AT+CK\r\n     
+#define F6188_VOLUME_DOWN "CL"             //  volume down   AT+CL\r\n     
+#define F6188_LANGUAGE_SWITCH  "CM"        //  Multi-language switch   AT+CM\r\n     
+#define F6188_CHANNEL_SWITCH "CO"          //  Channel switching (invalid)   AT+CO\r\n     to be tested
+#define F6188_SHUTDOWN "CP"                //  Shutdown  AT+CP\r\n     
+#define F6188_SWITCH_INPUT "CT"            //  Enter the test mode   AT+CT\r\n     
+#define F6188_OPEN_PHONE_VOICE "CV"        //  Open phone VOICE  AT+CV\r\n     
+#define F6188_MEMORY_CLEAR "CZ"            //  Memory clear  AT+CZ\r\n     
+#define F6188_LANGUAGE_SET_NUMBER "CMM"    //  Number:( 0-4 )  Set the number of multi-lingual   AT+CMM4\r\n     
+#define F6188_MUSIC_TOGGLE_PLAY_PAUSE "MA" //  Music Play / Pause  AT+MA\r\n     
+#define F6188_MUSIC_STOP "MC"              //  The music stops   AT+MC\r\n     
+#define F6188_MUSIC_NEXT_TRACK "MD"        //  next track  AT+MD\r\n     
+#define F6188_MUSIC_PREVIOUS_TRACK "ME"    //  previous track  AT+ME\r\n     
+#define F6188_MUSIC_FAST_FORWARD "MF"      //  fast forward  AT+MF\r\n     test how does this exacly works?
+#define F6188_MUSIC_REWIND "MH"            //  rewind  AT+MH\r\n     test how does this exacly works?
+#define F6188_GET_NAME "MN"                //  Query bluetooth name  AT+MN\r\n   NA:BK8000L\r\n  test this
+#define F6188_GET_CONNECTION_STATUS "MO"   //  Bluetooth connection status inquiry   AT+MO\rn  connection succeeded:" C1\r\n"no connection:"C0\r\n"  
+#define F6188_GET_PIN_CODE "MP"            //  PIN Code query  AT+MP\r\n   PN:0000\r\n   
+#define F6188_GET_ADDRESS "MR"             //  Query bluetooth address   AT+MR\r\n   AD:111111111111\r\n   
+#define F6188_GET_SOFTWARE_VERSION "MQ"    //  Query software version  AT+MQ\r\n   XZX-V1.2\r\n  
+#define F6188_MUSIC_GET_STATUS "MV "       //  Bluetooth playback status inquiry   AT+MV\r\n   Play: "MB\r\n", time out:"MA\r\n", disconnect:" M0\r\n"   
+#define F6188_GET_HFP_STATUS "MY"          //  Bluetooth inquiry HFP status  AT+MY\r\n   disconnect:"M0\r\n", connection:"M1\r\n", Caller: "M2\r\n", Outgoing: "M3\r\n", calling:"M4\r\n"  
+
+
+
+#if defined(USE_SW_SERIAL)
+#if ARDUINO >= 100
+#include <SoftwareSerial.h>
+#else
+#include <NewSoftSerial.h>
+#endif
+#endif
+
+
+class F6188
+{
+  public:
+
+    uint8_t BTState;
+    uint8_t CallState;
+    uint8_t MusicState;
+    uint8_t PowerState;
+
+    String CallerID;
+
+    enum STATES
+    {
+      Playing, //MA
+      Idle, //MB
+      IncomingCall, //IR- or M2
+      OutgoingCall, //PR- or M3
+      CallInProgress, //M4
+      Connected, // M1
+      Disconnected, //M0
+      On,
+      Off,
+      Pairing
+    };
+
+#if defined(USE_SW_SERIAL)
+#if ARDUINO >= 100
+    F6188(SoftwareSerial *ser);
+#else
+    F6188(NewSoftSerial  *ser);
+#endif
+#endif
+    F6188(HardwareSerial *ser);
+    void begin(uint32_t baudrate);
+    ~F6188();
+    uint8_t sendData(String cmd);
+    uint8_t getNextEventFromBT();
+
+    uint8_t PairingInit();
+    uint8_t PairingExit();
+    uint8_t ConnectLastDevice();
+    uint8_t Disconnect();
+    uint8_t callAnsware();
+    uint8_t callReject();
+    uint8_t callHangUp();
+    uint8_t callRedial();
+    uint8_t volumeUp();
+    uint8_t volumeDown();
+    uint8_t languageSwitch();
+    uint8_t channelSwitch();
+    uint8_t shutdown_BT();
+    uint8_t switchInput();
+    uint8_t open_phone_voice();
+    uint8_t memoryClear();
+    uint8_t language_set_number(uint8_t number);
+    uint8_t musicTogglePlayPause();
+    uint8_t musicStop();
+    uint8_t musicNextTrack();
+    uint8_t musicPreviousTrack();
+    uint8_t musicFastForward();
+    uint8_t musicRewind();
+    uint8_t getName();
+    uint8_t getConnectioStatus();
+    uint8_t getPinCode();
+    uint8_t getAddress();
+    uint8_t getSoftwareVersion();
+    uint8_t MusicGetStatus();
+    uint8_t getHFPstatus();
+
+    uint8_t decodeReceivedString(String receivedString);
+    String returnCallerID(String receivedString); //return number between ""
+    String returnBtModuleName(String receivedString); //return module name between : and \0
+    String BT_ADDR;
+    String BT_NAME;
+    uint16_t BT_PIN;
+
+  private:
+    void DBG(String text);
+
+#if  defined(USE_SW_SERIAL)
+#if ARDUINO >= 100
+    SoftwareSerial *btSwSerial;
+#else
+    NewSoftSerial  *btSwSerial;
+#endif
+#endif
+    HardwareSerial *btHwSerial;
+
+};
+
+
+#endif
---
+++ libraries/F-6188/example/F-6188.ino
@@ -0,0 +1,256 @@
+#include "F6188.h"
+#include <SoftwareSerial.h>
+
+uint8_t BTState;
+uint8_t CallState;
+uint8_t MusicState;
+uint8_t PowerState;
+
+SoftwareSerial swSerial(7, 6); //rxPin, txPin, inverse_logic
+
+F6188 BT(&swSerial);
+
+void setup() {
+  BT.begin(9600);
+  Serial.begin(115200);
+  Serial.println(F("press h for help"));
+  BT.getName();
+  BT.getConnectioStatus();
+  BT.getPinCode();
+  BT.getAddress();
+  BT.getSoftwareVersion();
+  BT.MusicGetStatus();
+  BT.getHFPstatus();
+}
+
+void loop() {
+
+  if (Serial.available() > 0)
+  {
+    // read the incoming byte:
+    char c = Serial.read();
+    switch (c)
+    {
+      case '1':
+        BT.PairingInit();
+        break;
+      case '2':
+        BT.PairingExit();
+        break;
+      case '3':
+        BT.ConnectLastDevice();
+        break;
+      case '4':
+        BT.Disconnect();
+        break;
+      case '5':
+        BT.callAnsware();
+        break;
+      case '6':
+        BT.callReject();
+        break;
+      case '7':
+        BT.callHangUp();
+        break;
+      case '8':
+        BT.callRedial();
+        break;
+      case '9':
+        BT.volumeUp();
+        break;
+      case 'a':
+        BT.volumeDown();
+        break;
+      case 'b':
+        BT.languageSwitch();
+        break;
+      case 'c':
+        BT.channelSwitch();
+        break;
+      case 'd':
+        BT.shutdown_BT();
+        break;
+      case 'e':
+        BT.switchInput();
+        break;
+      case 'f':
+        {
+          Serial.read(); //read \r
+          Serial.read(); //read \n
+          Serial.println(F("type command (without AT+) to by send:"));
+          String str;
+          c = 0;
+          while (!Serial.available()) {}; //wait until user input something ... then read it:
+          while (Serial.available() > 0) {
+            c = Serial.read();
+            if ( c == 0xD ) {
+              break;
+            } else {
+              str = str + c;
+            }
+          }
+          BT.sendData(str);
+        }
+        break;
+      case 'g':
+        BT.open_phone_voice();
+        break;
+      case 'h': //help
+        Serial.println(F("PairingInit                  1"));
+        Serial.println(F("PairingExit                  2"));
+        Serial.println(F("ConnectLastDevice            3"));
+        Serial.println(F("Disconnect                   4"));
+        Serial.println(F("callAnsware                  5"));
+        Serial.println(F("callReject                   6"));
+        Serial.println(F("callHangUp                   7"));
+        Serial.println(F("callRedial                   8"));
+        Serial.println(F("volumeUp                     9"));
+        Serial.println(F("volumeDown                   a"));
+        Serial.println(F("languageSwitch               b"));
+        Serial.println(F("channelSwitch                c"));
+        Serial.println(F("shutdown_BT                  d"));
+        Serial.println(F("switch input                 e"));
+        Serial.println(F("send custom AT+command       f"));
+        Serial.println(F("open_phone_voice             g"));
+        Serial.println(F("                 i"));
+        Serial.println(F("memoryClear                  j"));
+        Serial.println(F("language_set_number      k+num"));
+        Serial.println(F("musicTogglePlayPause         l"));
+        Serial.println(F("musicStop                    m"));
+        Serial.println(F("musicNextTrack               n"));
+        Serial.println(F("musicPreviousTrack           o"));
+        Serial.println(F("musicFastForward             q"));
+        Serial.println(F("musicRewind                  r"));
+        Serial.println(F("getName                      s"));
+        Serial.println(F("getConnectioStatus           t"));
+        Serial.println(F("getPinCode                   u"));
+        Serial.println(F("getAddress                   v"));
+        Serial.println(F("getSoftwareVersion           w"));
+        Serial.println(F("MusicGetStatus               x"));
+        Serial.println(F("getHFPstatus                 y"));
+        break;
+      case 'i':
+
+        break;
+      case 'j':
+        BT.memoryClear();
+        break;
+      case 'k':
+        {
+          Serial.read(); //read \r
+          Serial.read(); //read \n
+          Serial.println(F("give me number 0-4"));
+          char b;
+          c = 0;
+          while (!Serial.available()) {}; //wait until user input something ... then read it:
+          if (Serial.available() > 0) {
+            b = Serial.read();
+          }
+          BT.language_set_number(b - 0x30);
+          c = 0;
+        }
+        break;
+      case 'l':
+        BT.musicTogglePlayPause();
+        break;
+      case 'm':
+        BT.musicStop();
+        break;
+      case 'n':
+        BT.musicNextTrack();
+        break;
+      case 'o':
+        BT.musicPreviousTrack();
+        break;
+      case 'p':
+        BT.musicFastForward();
+        break;
+      case 'r':
+        BT.musicRewind();
+        break;
+      case 's':
+        BT.getName();
+        break;
+      case 't':
+        BT.getConnectioStatus();
+        break;
+      case 'u':
+        BT.getPinCode();
+        break;
+      case 'v':
+        BT.getAddress();
+        break;
+      case 'w':
+        BT.getSoftwareVersion();
+        break;
+      case 'x':
+        BT.MusicGetStatus();
+        break;
+      case 'y':
+        BT.getHFPstatus();
+        break;
+
+    }
+  }
+ 
+  BT.getNextEventFromBT();
+
+  if (BTState != BT.BTState) {
+    switch (BT.BTState) {
+      case BT.Connected:
+        Serial.println(F("Bluetooth connected"));
+        break;
+      case BT.Disconnected:
+        Serial.println(F("Bluetooth disconnected"));
+        break;
+    }
+    BTState = BT.BTState;
+  }
+
+  if (CallState != BT.CallState) {
+    switch (BT.CallState) {
+      case (BT.IncomingCall):
+        Serial.println(F("Incoming call:"));
+        Serial.println(BT.CallerID);
+        break;
+      case (BT.OutgoingCall):
+        Serial.println(F("Dialing:"));
+        Serial.println(BT.CallerID);
+        break;
+      case (BT.CallInProgress):
+        Serial.println(F("Calling:"));
+        Serial.println(BT.CallerID);
+        break;
+    }
+    CallState = BT.CallState;
+  }
+
+  if (MusicState != BT.MusicState) {
+    switch (BT.MusicState) {
+      case (BT.Playing):
+        Serial.println(F("Playing music"));
+        break;
+      case (BT.Idle):
+        Serial.println(F("Music stoped"));
+        break;
+    }
+    MusicState = BT.MusicState;
+  }
+
+  if (PowerState != BT.PowerState) {
+    switch (BT.PowerState) {
+      case (BT.On):
+        Serial.println(F("Module ON"));
+        Serial.print(F("NAME: "));Serial.println(BT.BT_NAME);
+        Serial.print(F("PIN: "));Serial.println(BT.BT_PIN);
+        break;
+      case (BT.Off):
+        Serial.println(F("Module OFF"));
+        break;
+    }
+    PowerState = BT.PowerState;
+  }
+
+}
+
+
